0cfe08b58abf3bf8cf5383ad030135823af46031
SHANKAR <84023210+shankarg1@users.noreply.github.com>
2022-06-10 21:10:04 +0530

    projectsp age

--- pages/Projects.js
+++ pages/Projects.js
@@ -14,46 +14,42 @@ function Projects() {
         </div>
         <div className="h-50  ml-5  sm:ml-20 grid-cols-1   sm:grid-col-3   sm:flex gap-2 content-start ...">
 
-            <div className="max-w-sm mt-5 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <div className="max-w-sm mt-5 border border-gray-200 rounded-lg shadow-md dark:border-gray-300">
                 <a href="https://netflix-clone-shankar.web.app/">
                     <img className="rounded-t-lg" src="https://cdn.vox-cdn.com/thumbor/Yq1Vd39jCBGpTUKHUhEx5FfxvmM=/39x0:3111x2048/1200x800/filters:focal(39x0:3111x2048)/cdn.vox-cdn.com/uploads/chorus_image/image/49901753/netflixlogo.0.0.png" alt="" />
                 </a>
+                <div className=''></div>
                 <div className="p-5">
                     <a href="https://netflix-clone-shankar.web.app/">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 font-Mono dark:text-white">Netflix Clone</h5>
+                        <h5 className="mb-2 ml-20 text-2xl font-bold tracking-tight text-gray-900 font-Mono dark:text-white">Netflix Clone</h5>
                     </a>
-                    <p className="mb-3 font-bold text-gray-400 font-Mono dark:text-gray-400">Netflix Clone is an online video entertainment website similar to Netflix,
+                    <p className="mb-3 font-bold text-center text-gray-400 font-Mono dark:text-gray-400">Netflix Clone is an online video entertainment website similar to Netflix,
                     </p>
                     <p className='font-semibold text-Mono dark:text-gray-400'>Credentials:</p>
-                    
-                    <p className='font-semibold text-Mono dark:text-gray-400'>user@example.com <br/> 1234567890</p>
-                    <a href="https://netflix-clone-shankar.web.app/"  className="inline-flex items-center px-3 py-2 mt-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Visit
 
-                    </a>
+                    <p className='font-semibold text-Mono dark:text-gray-400'>user@example.com <br /> 1234567890</p>
+
                 </div>
             </div>
 
 
-
-
-
-            <div className="max-w-sm mt-5 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-                <a href="#">
+            <div className="max-w-sm mt-5 border border-gray-200 rounded-lg shadow-md dark:border-gray-300">
+                <a href="https://netflix-clone-shankar.web.app/">
                     <img className="rounded-t-lg" src="https://cdn.vox-cdn.com/thumbor/Yq1Vd39jCBGpTUKHUhEx5FfxvmM=/39x0:3111x2048/1200x800/filters:focal(39x0:3111x2048)/cdn.vox-cdn.com/uploads/chorus_image/image/49901753/netflixlogo.0.0.png" alt="" />
                 </a>
+                <div className=''></div>
                 <div className="p-5">
-                    <a href="#">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 font-Mono dark:text-white">Crypto Tracker</h5>
+                    <a href="https://netflix-clone-shankar.web.app/">
+                        <h5 className="mb-2 ml-20 text-2xl font-bold tracking-tight text-gray-900 font-Mono dark:text-white">Netflix Clone</h5>
                     </a>
-                    <p className="mb-3 font-bold text-gray-700 font-Mono dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Visit
+                    <p className="mb-3 font-bold text-center text-gray-400 font-Mono dark:text-gray-400">Netflix Clone is an online video entertainment website similar to Netflix,
+                    </p>
+                    <p className='font-semibold text-Mono dark:text-gray-400'>Credentials:</p>
 
-                    </a>
+                    <p className='font-semibold text-Mono dark:text-gray-400'>user@example.com <br /> 1234567890</p>
+
                 </div>
             </div>
-
 
 
 
