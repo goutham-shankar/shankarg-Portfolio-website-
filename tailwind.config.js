5c2cacaa738f5625a4630ddb8921553a0a2e675d
GOUTHAM SANKAR <84023210+goutham-shankar@users.noreply.github.com>
2023-07-11 13:06:42 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        'custom-gray':'#444945',
-    
       fontFamily: {
         Rampart: ["Cedarville Cursive", "cursive"],
         Mono: ["Spline Sans Mono", "monospace"],
       },
-    },
     },
   },
   plugins: [
